1bfe25e8c6e45822ee3281f6977a9bb0024302f6
jorgeeyy <jqubez@gmail.com>
2026-01-11 16:14:01 +0000

    minor changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-# webhook_url = "https://webhook.site/8a64fb24-3ff4-47b1-9394-be3df1e0eee2"
+# webhook_url = "your-webhook-url"
 webhook_url = "http://127.0.0.1:5000/webhook"
 
 data = {
